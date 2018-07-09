a8a1caa2e8fc8f1b0946ebff81a3fcb2e6c9e664
Antonio Menarde <amenarde@gmail.com>
2018-07-09 15:10:42 -0700

    adds service fabric data mocks used for testing

---
+++ src/Microsoft.ServiceFabric.Data.Mocks/MockAsyncEnumerable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Microsoft.ServiceFabric.Data.Mocks
+{
+	public sealed class MockAsyncEnumerable<T> : IAsyncEnumerable<T>
+	{
+		private readonly IEnumerable<T> _enumerable;
+
+		public MockAsyncEnumerable(IEnumerable<T> enumerable)
+		{
+			_enumerable = enumerable ?? throw new ArgumentNullException(nameof(enumerable));
+		}
+
+		public IAsyncEnumerator<T> GetAsyncEnumerator()
+		{
+			return new MockAsyncEnumerator<T>(_enumerable.GetEnumerator());
+		}
+	}
+}
---
+++ src/Microsoft.ServiceFabric.Data.Mocks/MockTransaction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.ServiceFabric.Data.Mocks
+{
+	public class MockTransaction : ITransaction
+	{
+		private static long _sequenceNumber = 0;
+
+		public long CommitSequenceNumber { get; private set; } = -1;
+
+		public long TransactionId { get; } = Interlocked.Increment(ref _sequenceNumber);
+
+		void ITransaction.Abort()
+		{
+		}
+
+		Task ITransaction.CommitAsync()
+		{
+			CommitSequenceNumber = Interlocked.Increment(ref _sequenceNumber);
+			return Task.CompletedTask;
+		}
+
+		void IDisposable.Dispose()
+		{
+		}
+
+		Task<long> ITransaction.GetVisibilitySequenceNumberAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
