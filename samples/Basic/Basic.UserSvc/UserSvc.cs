9442de3f3a57659c54cf1f4f88b548c5802bd836
Antonio Menarde <amenarde@gmail.com>
2018-08-02 15:11:06 -0700

    adds comment to UserSvc example and removes dependency to the old indexing in sample

--- samples/Basic/Basic.UserSvc/UserSvc.cs
+++ samples/Basic/Basic.UserSvc/UserSvc.cs
@@ -13,7 +13,7 @@ using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 using Basic.Common;
-using Microsoft.ServiceFabric.Data.Indexing.Persistent;
+using ServiceFabric.Extensions.Data.Indexing.Persistent;
 using ServiceFabric.Extensions.Services.Queryable.LINQ;
 
 namespace Basic.UserSvc
@@ -56,10 +56,18 @@ namespace Basic.UserSvc
 
 		protected override async Task RunAsync(CancellationToken cancellationToken)
 		{
+
+            // Make a traditional IReliableDictionary <UserName, UserProfile>
 			var users = await StateManager.GetOrAddAsync<IReliableDictionary<UserName, UserProfile>>("users");
+
+            // Make an IReliableIndexedDictionary <UserName, UserProfile>, with two indices:
+            // Index<UserProfile.Email, UserName[]> and Index<UserProfile.Age, UserName[]>
             var indexed_users = await StateManager.GetOrAddIndexedAsync<UserName, UserProfile>("indexed_users",
                  FilterableIndex<UserName, UserProfile, string>.CreateQueryableInstance("Email"),
-                   FilterableIndex<UserName, UserProfile, int>.CreateQueryableInstance("Age"));
+                 FilterableIndex<UserName, UserProfile, int>.CreateQueryableInstance("Age"));
+
+            // The above dictionaries have the same content (below), but because the second has indices,
+            // Linq queries and external queries should use that and be faster.
 
             for (int i = 0; i < 100; i++)
 			{
@@ -92,7 +100,6 @@ namespace Basic.UserSvc
             /* Example of LINQ querying on IReliableIndexedDictionary
              
                Sometimes you will want your application code to carry out queries against your RC as well, and LINQ is a great way to do so
-               It is important to note that currently the query must return type TValue, this is a bug
 
                Also, when you write your query, make sure to put all your WHERE logic into a single WHERE statement
                since each statement carries its own context the indexing middleware cannot efficiently operate on disjoint statements,
@@ -100,10 +107,9 @@ namespace Basic.UserSvc
                          qdict.Where(x => x.Email == "user-0@example.com").Where(x => x.Age <= 20)
             */
 
-            /* Remove comments to run Linq
             
             // Create LINQ-Queryable state of IndexedDictionary
-            QueryableReliableIndexedDictionary<UserName, UserProfile, UserProfile> qdict = new QueryableReliableIndexedDictionary<UserName, UserProfile, UserProfile>(indexed_users, StateManager);
+            var qdict = new QueryableReliableIndexedDictionary<UserName, UserProfile, UserProfile>(indexed_users, StateManager);
 
             // Create the same query two different ways
             var query = from UserProfile profile in qdict
@@ -122,7 +128,6 @@ namespace Basic.UserSvc
 
             }
             
-            */
         }
     }
 }
